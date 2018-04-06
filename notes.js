0aec35e862885471c85568ce09a995ed1e5f14d2
Nikesh Jha <jha.nikesh@hotmail.com>
2018-04-06 23:47:44 +0545

    code refactoring and completion of read,delete,save operation

--- notes.js
+++ notes.js
@@ -3,28 +3,99 @@ const fs = require('fs');
 
 
 const addNote = (title, body) => {
+    //create a note object from title and body arguments
     const note = {
         title,
         body
     }
-    let notes = []
-    let notesString = fs.readFileSync('notes-data.json')
-    notes = JSON.parse(notesString);
-    notes.push(note)
-    fs.writeFileSync("notes-data.json",JSON.stringify(notes));
+    const notes = readAllNotes();
+    const duplicateNotes = notes.filter((note) => note.title === title); //check for duplicate note
+    if (duplicateNotes.length === 0) {
+        notes.push(note)
+        saveNote(notes);
+    } else {
+        console.log('duplicate note !!');
+    }
+
 };
 
+
+//Save notes to file
+const saveNote = (notes) => {
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+}
+
+
+const readAllNotes = () => {
+
+    let notes = []
+    try {
+        let notesString = fs.readFileSync('notes-data.json')
+        notes = JSON.parse(notesString);
+        return notes;
+    } catch (e) {
+        console.log('Something wrong happened: ' + e);
+        return [];
+    }
+
+}
+
+
+
+/**
+ * 
+ * 
+ * @returns all the notes [notes]
+ */
 const getAll = () => {
-    console.log('getting all notes');
+    console.log(':::notes:::');
+    console.log(readAllNotes());
+    return readAllNotes();
 }
 
-const remove = (title) =>{
-    console.log('removing note: ', title);
+
+
+/**
+ * 
+ * 
+ * @param {any} title 
+ * @returns 
+ */
+const remove = (title) => {
+    let allNotes = readAllNotes();
+    console.log('\n All notes before deletion:');
+    console.log(allNotes);
+    let filteredNotes = allNotes.filter((note) => {
+        return note.title != title;
+    })
+    console.log('\n After deletion:')
+    console.log(filteredNotes);
+    saveNote(filteredNotes);
+    return filteredNotes;
 }
 
-const read = (title) =>{
+
+/**
+ * 
+ * 
+ * @param {any} title 
+ * @returns note object
+ */
+const read = (title) => {
     console.log('reading note: ', title);
+    const allNotes = getAll();
+    const note = allNotes.filter((note) => {
+        return note.title === title;
+    })
+    if (note.length == 1) {
+        console.log('the retreived note is: ' + note[0].title + " " + note[0].body);
+    } else {
+        console.log('not found solti !!!');
+    }
+
+    return note;
 }
+
 
 module.exports = {
     addNote,
